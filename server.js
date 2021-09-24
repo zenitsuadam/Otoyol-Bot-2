3293735b3fbd5acc3c01e53580cecfdd64cd789d
Glitch (glitch-blank-node) <none>
2021-09-24 09:05:45 +0000

    🏆🏊🏼 Checkpoint ./src/pages/index.hbs:36586366/774 ./public/style.css:36586366/371 ./package.json:36586366/725 ./server.js:36586366/5556

--- server.js
+++ server.js
@@ -1,52 +1,21 @@
-const path = require("path");
-
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // set this to true for detailed logging:
-  logger: false
-});
-
-// Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
-});
-
-// fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
-
-// point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
-  engine: {
-    handlebars: require("handlebars")
-  }
-});
-
-// Our main GET home page route, pulls from src/pages/index.hbs
-fastify.get("/", function(request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = {
-    greeting: "Hello Node!"
-  };
-  // request.query.paramName <-- a querystring example
-  reply.view("/src/pages/index.hbs", params);
-});
-
-// A POST route to handle form submissions
-fastify.post("/", function(request, reply) {
-  let params = {
-    greeting: "Hello Form!"
-  };
-  // request.body.paramName <-- a form post example
-  reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(process.env.PORT, function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
-});
+const Aoijs = require("aoi.js")
+var fs = require('fs')
+const bot = new Aoijs.Bot({
+    token: process.env['TOKEN'],
+    prefix: "PREFIX"
+})
+bot.onMessage()
+var reader = fs.readdirSync("./komutlar/").filter(file => file.endsWith(".js"))
+for(const file of reader) {    
+    const command = require(`./komutlar/${file}`)
+    bot.command({
+        name: command.name,
+        code: command.code
+    })
+} 
+bot.status({
+  text: "DARKLON #CODE", // DURUMDA YAZACAĞI METİN
+  type: "PLAYING", 
+  status: "online/dnd/idle", // BOTUN DURUMU
+  time: 12 // DURUM DEĞİŞME SÜRESİ
+})
